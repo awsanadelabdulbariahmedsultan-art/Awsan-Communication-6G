f8d3b7c3a358f8f8997161523d1a7a8a0962b472
Awsan Adel Abdulbari Ahmed Sultan <awsan.sultan@gmail.com>
2026-05-31 19:18:58 +0300

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,1 +1,7 @@
-.
+#!/bin/bash
+echo "--- بدء المزامنة العالمية لمشروع Awsan Communication ---"
+git add .
+timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+git commit -m "Continuous Sync by Eng. Awsan - $timestamp"
+git push origin main
+echo "--- تمت المزامنة والحفظ في GitHub بنجاح ---"
