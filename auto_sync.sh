25bc94caa7967695149ee815481b62110ee14156
Awsan Adel Abdulbari Ahmed Sultan <awsan.sultan@gmail.com>
2026-05-31 19:58:09 +0300

    Update auto_sync.sh

--- auto_sync.sh
+++ auto_sync.sh
@@ -1,1 +1,12 @@
-.
+#!/bin/bash
+echo "--- نظام المزامنة الآلي للمهندس أوسان يعمل الآن ---"
+while true; do
+  git add .
+  if ! git diff-index --quiet HEAD; then
+    timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+    git commit -m "Automated Update: $timestamp (Eng. Awsan Tech Suite)"
+    git push origin main
+    echo "Done: تم الرفع التلقائي لـ GitHub بنجاح في $timestamp"
+  fi
+  sleep 10 # يفحص المجلد كل 10 ثوانٍ لضمان الاستمرارية
+done
